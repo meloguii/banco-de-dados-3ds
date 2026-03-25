07add133240d1e281ac473f85ca0862dcf9bbb3c
meloguii <guilherme73184@gmail.com>
2026-03-25 20:39:34 -0300

    Add sample clients and purchases data
    Inserted sample data for clients and purchases.

--- sistema-livraria.sql
+++ sistema-livraria.sql
@@ -16,3 +16,22 @@ CREATE TABLE Compras (
     NomeLivro VARCHAR(150) NOT NULL,
     FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
 );
+
+SELECT * FROM Clientes;
+
+-- Inserindo clientes
+INSERT INTO Clientes (nomeCliente, emailCliente)
+VALUES 
+('Ana Silva', 'ana.silva@email.com'),
+('Carlos Souza', 'carlos.souza@email.com'),
+('Mariana Oliveira', 'mariana.oliveira@email.com'),
+('João Pereira', 'joao.pereira@email.com');
+
+-- Inserindo compras
+INSERT INTO Compras (ClienteID, NomeLivro)
+VALUES
+(1, 'Dom Casmurro'),
+(2, 'O Senhor dos Anéis'),
+(3, '1984'),
+(1, 'A Revolução dos Bichos'),
+(4, 'Harry Potter e a Pedra Filosofal');
